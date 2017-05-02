42e03e30fac65e738415bb90295d6b84169fdeb5
Micooz <micooz@hotmail.com>
2017-05-02 17:35:09 +0800

    feat(packaging): add build.sh to pack binary files

---
+++ packaging/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+node_modules/.bin/electron-packager ./ blinksocks-desktop \
+  --platform="win32" --arch="x64" \
+  --app-copyright="Copyright 2017 blinksocks" \
+  --app-version="0.0.1" \
+  --asar \
+  --icon="packaging/icon.png" \
+  --electron-version="1.6.8" \
+  --overwrite \
+  --win32metadata.CompanyName="" \
+  --win32metadata.FileDescription="Cross-platform desktop GUI for blinksocks" \
+  --win32metadata.OriginalFilename="blinksocks-desktop" \
+  --win32metadata.ProductName="blinksocks-desktop" \
+  --win32metadata.InternalName="" \
+  --win32metadata.requestedExecutionLevel="user"

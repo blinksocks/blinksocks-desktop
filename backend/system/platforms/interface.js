3f2f5afe9c0e983ac86546ac3b34a718db9a94a7
Micooz Lee <micooz@hotmail.com>
2017-05-23 23:23:06 +0800

    chore(ISysProxy): make some logs

--- backend/system/platforms/interface.js
+++ backend/system/platforms/interface.js
@@ -1,19 +1,19 @@
 module.exports = class ISysProxy {
 
   async setGlobal(/* {host, port, bypass} */) {
-
+    console.info('abstract method ISysProxy.setGlobal() is called, perhaps you should make an implementation.');
   }
 
   async setPAC(/* {url} */) {
-
+    console.info('abstract method ISysProxy.setPAC() is called, perhaps you should make an implementation.');
   }
 
   async restoreGlobal(/* {host, port, bypass} */) {
-
+    console.info('abstract method ISysProxy.restoreGlobal() is called, perhaps you should make an implementation.');
   }
 
   async restorePAC(/* {url} */) {
-
+    console.info('abstract method ISysProxy.restorePAC() is called, perhaps you should make an implementation.');
   }
 
 };

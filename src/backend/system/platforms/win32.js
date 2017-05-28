525c3af954361dc47119d4cf5074d749d5758bd9
Micooz Lee <micooz@hotmail.com>
2017-05-28 15:39:52 +0800

    chore(defs): add more default bypass

--- src/backend/system/platforms/win32.js
+++ src/backend/system/platforms/win32.js
@@ -65,17 +65,6 @@ module.exports = class Win32SysProxy extends ISysProxy {
 
   async setGlobal({host, port, bypass}) {
     if (host && port) {
-      // TODO: remove hardcode
-      const bypass = [
-        '<local>',
-        'localhost', '127.*', '10.*',
-        '172.16.*', '172.17.*', '172.18.*',
-        '172.19.*', '172.20.*', '172.21.*',
-        '172.22.*', '172.23.*', '172.24.*',
-        '172.25.*', '172.26.*', '172.27.*',
-        '172.28.*', '172.29.*', '172.30.*',
-        '172.31.*', '172.32.*', '192.168.*'
-      ];
       await exec(`${this._agent} global ${host}:${port} "${bypass.join(';')}"`);
     }
   }
--- src/defs/bs-config-template.js
+++ src/defs/bs-config-template.js
@@ -17,7 +17,16 @@ module.exports.DEFAULT_CONFIG_STRUCTURE = {
   profile: false,
   watch: false,
   log_level: 'info',
-  bypass: ['127.0.0.1', '::1', 'localhost'],
+  bypass: [
+    '127.0.0.1', '::1', '<local>',
+    'localhost', '127.*', '10.*',
+    '172.16.*', '172.17.*', '172.18.*',
+    '172.19.*', '172.20.*', '172.21.*',
+    '172.22.*', '172.23.*', '172.24.*',
+    '172.25.*', '172.26.*', '172.27.*',
+    '172.28.*', '172.29.*', '172.30.*',
+    '172.31.*', '172.32.*', '192.168.*'
+  ],
   pac: 'http://localhost:1090/proxy.pac',
   pac_status: 0,
   app_status: 0

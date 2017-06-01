88f2831fc0d5d0ed3d833090426e39fa3106aeab
Micooz <micooz@hotmail.com>
2017-06-01 10:21:32 +0800

    fix(constants): shared constants must declared first

--- src/backend/constants.js
+++ src/backend/constants.js
@@ -1,9 +1,13 @@
 const os = require('os');
 const path = require('path');
 
+const HOME_DIR = os.homedir();
+const BLINKSOCKS_DIR = path.join(HOME_DIR, '.blinksocks');
+
 module.exports = {
-  HOME_DIR: os.homedir(),
-  BLINKSOCKS_DIR: path.join(HOME_DIR, '.blinksocks'),
+  HOME_DIR,
+  BLINKSOCKS_DIR,
+  APP_HOME: path.resolve(__dirname, '..', '..'),
   DEFAULT_GFWLIST_PATH: path.join(BLINKSOCKS_DIR, 'gfwlist.txt'),
   DEFAULT_CONFIG_FILE: path.join(BLINKSOCKS_DIR, 'blinksocks.client.js'),
   GFWLIST_URL: 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt',

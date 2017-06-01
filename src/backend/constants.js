e6f35b0acf9016a1910f4bc42cb631425b2385e2
Micooz <micooz@hotmail.com>
2017-06-01 10:46:03 +0800

    chore(constants): add LOG_DIR and LOG_FILE_PATH

--- src/backend/constants.js
+++ src/backend/constants.js
@@ -3,10 +3,13 @@ const path = require('path');
 
 const HOME_DIR = os.homedir();
 const BLINKSOCKS_DIR = path.join(HOME_DIR, '.blinksocks');
+const LOG_DIR = ath.join(BLINKSOCKS_DIR, 'logs');
 
 module.exports = {
   HOME_DIR,
   BLINKSOCKS_DIR,
+  LOG_DIR,
+  LOG_FILE_PATH: path.join(LOG_DIR, 'blinksocks-desktop.log'),
   APP_HOME: path.resolve(__dirname, '..', '..'),
   DEFAULT_GFWLIST_PATH: path.join(BLINKSOCKS_DIR, 'gfwlist.txt'),
   DEFAULT_CONFIG_FILE: path.join(BLINKSOCKS_DIR, 'blinksocks.client.js'),
--- src/backend/helpers/logger.js
+++ src/backend/helpers/logger.js
@@ -1,19 +1,5 @@
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
 const winston = require('winston');
-
-const HOME_DIR = os.homedir();
-const BLINKSOCKS_DIR = path.join(HOME_DIR, '.blinksocks');
-const LOG_PATH = path.join(BLINKSOCKS_DIR, 'logs');
-
-try {
-  fs.lstatSync(LOG_PATH);
-} catch (err) {
-  if (err.code === 'ENOENT') {
-    fs.mkdirSync(LOG_PATH);
-  }
-}
+const {LOG_FILE_PATH} = require('../constants');
 
 module.exports = new (winston.Logger)({
   level: 'silly',
@@ -23,7 +9,7 @@ module.exports = new (winston.Logger)({
       prettyPrint: true
     }),
     new (winston.transports.File)({
-      filename: path.join(LOG_PATH, 'blinksocks-desktop.log'),
+      filename: LOG_FILE_PATH,
       maxsize: 2 * 1024 * 1024, // 2MB
     })
   ]

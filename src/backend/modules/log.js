b328655ea2f068f78cb15a36c4068a7c11ee282c
Micooz <micooz@hotmail.com>
2017-06-10 09:43:24 +0800

    fix(modules): resolve an empty array when logger is empty

--- src/backend/modules/log.js
+++ src/backend/modules/log.js
@@ -17,21 +17,25 @@ const {
  */
 function query(logger, options) {
   return new Promise((resolve, reject) => {
-    const defaultOpts = {
-      from: new Date() - 24 * 60 * 60 * 1000,
-      until: new Date(),
-      limit: 100,
-      start: 0,
-      order: 'desc'
-    };
-    const opt = options || defaultOpts;
-    logger.query(opt, function (err, results) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    });
+    if (!logger) {
+      resolve([]);
+    } else {
+      const defaultOpts = {
+        from: new Date() - 24 * 60 * 60 * 1000,
+        until: new Date(),
+        limit: 100,
+        start: 0,
+        order: 'desc'
+      };
+      const opt = options || defaultOpts;
+      logger.query(opt, function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results.file);
+        }
+      });
+    }
   });
 }
 
@@ -40,7 +44,7 @@ module.exports = function logModule({bsLogger, bsdLogger}) {
   async function getBSLog(e, options) {
     try {
       const logs = await query(bsLogger, options);
-      e.sender.send(MAIN_QUERY_BS_LOG, {logs: logs.file});
+      e.sender.send(MAIN_QUERY_BS_LOG, {logs});
     } catch (err) {
       e.sender.send(MAIN_ERROR, err.message);
     }
@@ -49,7 +53,7 @@ module.exports = function logModule({bsLogger, bsdLogger}) {
   async function getBSDLog(e, options) {
     try {
       const logs = await query(bsdLogger, options);
-      e.sender.send(MAIN_QUERY_BSD_LOG, {logs: logs.file});
+      e.sender.send(MAIN_QUERY_BSD_LOG, {logs});
     } catch (err) {
       e.sender.send(MAIN_ERROR, err.message);
     }

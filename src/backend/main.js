0a77686e3243a1c6956a272d2a95257ced450a4c
Micooz <micooz@hotmail.com>
2017-06-10 20:02:20 +0800

    fix(main): ReferenceError: win is not defined

--- src/backend/main.js
+++ src/backend/main.js
@@ -223,7 +223,7 @@ app.on('ready', async () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (mainWindow === null) {
     createWindow();
   }
 });
